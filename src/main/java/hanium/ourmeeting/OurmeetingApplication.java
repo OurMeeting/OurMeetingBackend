eb4ca7f770198046a0534c0fe458b1fc643c4189
hanbin <leehan0617@naver.com>
2022-03-30 14:41:52 +0900

    inital commit

---
+++ src/main/java/hanium/ourmeeting/OurmeetingApplication.java
@@ -0,0 +1,13 @@
+package hanium.ourmeeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurmeetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurmeetingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hanium/ourmeeting/OurmeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package hanium.ourmeeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurmeetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
