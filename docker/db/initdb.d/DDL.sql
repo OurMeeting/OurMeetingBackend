ba7c3bd9c6566bd546611cf8bba97ef0306cf08e
hanbin <leehan0617@naver.com>
2022-04-02 01:37:52 +0900

    FIX: docker 설정 오류 수정

--- docker/db/initdb.d/DDL.sql
+++ docker/db/initdb.d/DDL.sql
@@ -1,1 +1,9 @@
-create database our_meeting;
+create database if not exists our_meeting;
+
+use our_meeting;
+
+create table if not exists test_table (
+    id int primary key auto_increment,
+    name VARCHAR(100) not null,
+    reg_dt date
+);
